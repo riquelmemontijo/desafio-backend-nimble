a051bcda5b271a84c82df48b12f32a7d96410c6a
Riquelme Montijo <riquelme.montijo@pm.me>
2025-10-16 15:48:48 -0300

    Criação do projeto

---
+++ src/main/java/br/com/nimblebaas/DesafioBackendNimbleApplication.java
@@ -0,0 +1,13 @@
+package br.com.nimblebaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendNimbleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioBackendNimbleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/nimblebaas/DesafioBackendNimbleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nimblebaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendNimbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
