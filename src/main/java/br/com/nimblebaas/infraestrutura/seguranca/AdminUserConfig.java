0b906aeabce19be6442d0069a9391a09c582cb1d
Riquelme Montijo <riquelme.montijo@pm.me>
2025-10-17 17:54:19 -0300

    feat: cria rotina para inserir um usuário administrador padrão

---
+++ src/main/java/br/com/nimblebaas/infraestrutura/seguranca/AdminUserConfig.java
@@ -0,0 +1,42 @@
+package br.com.nimblebaas.infraestrutura.seguranca;
+
+import br.com.nimblebaas.usuario.Role;
+import br.com.nimblebaas.usuario.RoleRepository;
+import br.com.nimblebaas.usuario.Usuario;
+import br.com.nimblebaas.usuario.UsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+
+    private final UsuarioRepository usuarioRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AdminUserConfig(UsuarioRepository usuarioRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        var roleAdmin = roleRepository.findByDescricao(Role.RoleValues.ADMIN.name());
+        var usuarioAdmin = usuarioRepository.findById(1L);
+        usuarioAdmin.ifPresentOrElse(
+                user -> {
+                    System.out.println("Usuario já existe");
+                },
+                () -> {
+                    var usuario = new Usuario(null, "admin@admin.com", "11122233344", "Admnistrator", passwordEncoder.encode("senha"), Set.of(roleAdmin));
+                    usuarioRepository.save(usuario);
+                });
+    }
+}
