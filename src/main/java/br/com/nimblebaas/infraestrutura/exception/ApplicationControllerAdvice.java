e8b9ccc5622e10fe068529228a9d16f1ba606c81
Riquelme Montijo <riquelme.montijo@pm.me>
2025-10-17 22:40:59 -0300

    feat: adiciona controlle e respostas corretas as requisições em caso de lançamento de exceptions

---
+++ src/main/java/br/com/nimblebaas/infraestrutura/exception/ApplicationControllerAdvice.java
@@ -0,0 +1,41 @@
+package br.com.nimblebaas.infraestrutura.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        var fieldErrors = e.getFieldErrors()
+                .stream()
+                .map(f -> new ParametroInvalido(f.getField(), f.getDefaultMessage()))
+                .toList();
+
+        var problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+
+        problemDetail.setTitle("Os parâmetros da requisição são inválidos");
+        problemDetail.setProperty("Parâmetros inválidos", fieldErrors);
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ProblemDetail handleNotFound(BadCredentialsException ex) {
+        var problemDetail = ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED);
+        problemDetail.setTitle("Unauthorized");
+        problemDetail.setDetail("Credenciais inválidas.");
+        return problemDetail;
+    }
+
+    private record ParametroInvalido(String nome, String motivo){}
+
+}
