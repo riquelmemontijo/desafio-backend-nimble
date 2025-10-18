223f31aa03cb2049c6b24fde2a1f0ed24ea57175
Riquelme Montijo <riquelme.montijo@pm.me>
2025-10-18 10:15:49 -0300

    feat: adiciona e mapeia entidade Cobranca

---
+++ src/main/java/br/com/nimblebaas/cobranca/CobrancaRepository.java
@@ -0,0 +1,8 @@
+package br.com.nimblebaas.cobranca;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CobrancaRepository extends JpaRepository<Cobranca, Long> {
+}
---
+++ src/main/java/br/com/nimblebaas/cobranca/StatusCobranca.java
@@ -0,0 +1,7 @@
+package br.com.nimblebaas.cobranca;
+
+public enum StatusCobranca {
+    PENDENTE,
+    PAGA,
+    CANCELADA
+}
