90163a1761a842ee0b2fc0061f48acccd5e09cf9
Riquelme Montijo <riquelme.montijo@pm.me>
2025-10-19 19:37:47 -0300

    feat: retira algoritmo de Luhn da validacao do número de cartao de credito

--- src/main/java/br/com/nimblebaas/infraestrutura/validacao/numcartao/CartaoDeCreditoValidator.java
+++ src/main/java/br/com/nimblebaas/infraestrutura/validacao/numcartao/CartaoDeCreditoValidator.java
@@ -12,42 +12,12 @@ public class CartaoDeCreditoValidator implements ConstraintValidator<CartaoCredi
             return false;
         }
 
-        // Remove espaços e hífens
         String numeroLimpo = numero.replaceAll("[\\s-]", "");
 
-        // Verifica se contém apenas dígitos
         if (!numeroLimpo.matches("\\d+")) {
             return false;
         }
 
-        // Verifica se tem entre 13 e 19 dígitos (tamanho padrão de cartões)
-        if (numeroLimpo.length() < 13 || numeroLimpo.length() > 19) {
-            return false;
-        }
-
-        // Aplica o algoritmo de Luhn
-        return validarAlgoritmoLuhn(numeroLimpo);
-    }
-
-    private boolean validarAlgoritmoLuhn(String numero) {
-        int soma = 0;
-        boolean alternar = false;
-
-        // Percorre o número de trás para frente
-        for (int i = numero.length() - 1; i >= 0; i--) {
-            int digito = Character.getNumericValue(numero.charAt(i));
-
-            if (alternar) {
-                digito *= 2;
-                if (digito > 9) {
-                    digito -= 9;
-                }
-            }
-
-            soma += digito;
-            alternar = !alternar;
-        }
-
-        return (soma % 10) == 0;
+        return numeroLimpo.length() >= 13 && numeroLimpo.length() <= 19;
     }
 }
