935de4b7db2c0d581d34ff132ee4cafdfd8a25cb
Riquelme Montijo <riquelme.montijo@pm.me>
2025-10-22 18:47:13 -0300

    refactor: modifica tipo string do DTO para Enum

--- src/main/java/br/com/nimblebaas/domain/cobranca/model/dto/CobrancaResponseDTO.java
+++ src/main/java/br/com/nimblebaas/domain/cobranca/model/dto/CobrancaResponseDTO.java
@@ -1,19 +1,22 @@
 package br.com.nimblebaas.domain.cobranca.model.dto;
 
 import br.com.nimblebaas.domain.cobranca.model.Cobranca;
+import br.com.nimblebaas.domain.cobranca.model.StatusCobranca;
 import br.com.nimblebaas.domain.usuario.model.dto.UsuarioCobrancaResponseDTO;
+
+import java.math.BigDecimal;
 
 public record CobrancaResponseDTO(Long id,
                                   UsuarioCobrancaResponseDTO originador,
                                   UsuarioCobrancaResponseDTO destinatario,
                                   String descricao,
-                                  String status,
-                                  String valor) {
+                                  StatusCobranca status,
+                                  BigDecimal valor) {
     public CobrancaResponseDTO(Cobranca cobranca) {
         this(cobranca.getId(),
              new UsuarioCobrancaResponseDTO(cobranca.getOriginador()),
              new UsuarioCobrancaResponseDTO(cobranca.getDestinatario()),
-             cobranca.getDescricao(), cobranca.getStatusCobranca().name(),
-             cobranca.getValor().toString());
+             cobranca.getDescricao(), cobranca.getStatusCobranca(),
+             cobranca.getValor());
     }
 }
