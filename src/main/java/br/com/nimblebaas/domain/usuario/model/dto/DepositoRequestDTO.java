17995fcf5c1b8ed08bdbd8921c51ba53033f505f
Riquelme Montijo <riquelme.montijo@pm.me>
2025-10-23 02:46:24 -0300

    fix: adiciona annotation not null no dto de request de deposito

--- src/main/java/br/com/nimblebaas/domain/usuario/model/dto/DepositoRequestDTO.java
+++ src/main/java/br/com/nimblebaas/domain/usuario/model/dto/DepositoRequestDTO.java
@@ -1,10 +1,13 @@
 package br.com.nimblebaas.domain.usuario.model.dto;
 
 import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
-public record DepositoRequestDTO(@Positive @Digits(integer = 10, fraction = 2)
+public record DepositoRequestDTO(@Positive
+                                 @NotNull
+                                 @Digits(integer = 10, fraction = 2)
                                  BigDecimal valor) {
 }
