dfee72c115b1d00c2639cb1d3bd77a69791cdda9
Riquelme Montijo <riquelme.montijo@pm.me>
2025-10-23 02:45:32 -0300

    fix: corrige bug de conflito entre hibernate e jpa na inicializacao da aplicacao

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT IGNORE INTO role (id, descricao) VALUES (1, 'admin');
-INSERT IGNORE INTO role (id, descricao) VALUES (2, 'basico');
+INSERT IGNORE INTO role (id, descricao) VALUES (1, 'ADMIN');
+INSERT IGNORE INTO role (id, descricao) VALUES (2, 'BASICO');
