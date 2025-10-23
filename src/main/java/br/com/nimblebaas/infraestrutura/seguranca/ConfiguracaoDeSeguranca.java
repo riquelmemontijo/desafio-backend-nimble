965096aa8f6d12f91a4c9bd7704292c907aa804f
Riquelme Montijo <riquelme.montijo@pm.me>
2025-10-23 20:44:30 -0300

    config: organiza requestMatchers

--- src/main/java/br/com/nimblebaas/infraestrutura/seguranca/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/nimblebaas/infraestrutura/seguranca/ConfiguracaoDeSeguranca.java
@@ -39,10 +39,10 @@ public class ConfiguracaoDeSeguranca {
 
         http.authorizeHttpRequests(auth -> auth
                         .requestMatchers("/usuarios/sign-in").permitAll()
+                        .requestMatchers("/usuarios/sign-up").permitAll()
                         .requestMatchers("/swagger-ui.html").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
-                        .requestMatchers("/usuarios/sign-up").permitAll()
                         .anyRequest().authenticated())
             .csrf(AbstractHttpConfigurer::disable)
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
